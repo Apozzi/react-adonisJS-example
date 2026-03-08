89a8b0d0da399d9989807c86821d2401e1f52340
Pozzi <eanderea1@gmail.com>
2026-03-08 12:21:33 -0300

    atualizar

--- backend/start/validator.ts
+++ backend/start/validator.ts
@@ -1,18 +1,7 @@
-/*
-|--------------------------------------------------------------------------
-| Validator file
-|--------------------------------------------------------------------------
-|
-| The validator file is used for configuring global transforms for VineJS.
-| The transform below converts all VineJS date outputs from JavaScript
-| Date objects to Luxon DateTime instances, so that validated dates are
-| ready to use with Lucid models and other parts of the app that expect
-| Luxon DateTime.
-|
-*/
 
 import { DateTime } from 'luxon'
-import { VineDate } from '@vinejs/vine'
+import vine, { VineDate, SimpleMessagesProvider } from '@vinejs/vine'
+import { RequestValidator } from '@adonisjs/core/http'
 
 declare module '@vinejs/vine/types' {
   interface VineGlobalTransforms {
@@ -21,3 +10,21 @@ declare module '@vinejs/vine/types' {
 }
 
 VineDate.transform((value) => DateTime.fromJSDate(value))
+
+const messagesProvider = new SimpleMessagesProvider({
+  'string': 'O campo {{ field }} deve ser uma string',
+  'email': 'O campo {{ field }} deve ser um endereço de e-mail válido',
+  'string.email': 'O campo {{ field }} deve ser um endereço de e-mail válido',
+  'minLength': 'O campo {{ field }} deve ter pelo menos {{ min }} caracteres',
+  'string.minLength': 'O campo {{ field }} deve ter pelo menos {{ min }} caracteres',
+  'maxLength': 'O campo {{ field }} não pode ter mais de {{ max }} caracteres',
+  'string.maxLength': 'O campo {{ field }} não pode ter mais de {{ max }} caracteres',
+  'database.unique': 'O valor informado para o campo {{ field }} já está em uso',
+  'password.sameAs': 'A confirmação de senha não confere',
+  'sameAs': 'A confirmação de senha não confere',
+  'required': 'O campo {{ field }} é obrigatório'
+})
+
+vine.messagesProvider = messagesProvider
+RequestValidator.messagesProvider = () => messagesProvider
+
